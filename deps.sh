d8341cd78c834e589b38bac0627d0d2dfb09cc3f
Andrew LaCharite <adlacharite@gmail.com>
2020-03-28 18:31:51 +0000

    fix bugs

--- deps.sh
+++ deps.sh
@@ -1,6 +1,6 @@
 
-addpt install lynx libglu1-mesa-dev libgtk2.0-0
+apt install lynx libglu1-mesa-dev libgtk2.0-0
 apt-get install libsdl1.2debian libsdl-image1.2 libsdl-ttf2.0-0 libopenal1 libsndfile1 libncursesw5
 lynx -cmd_script=getdf "http://dffd.bay12games.com/file.php?id=13244"
 
-tar -xv Lin*
+tar -xvf Lin*
