8f812f4a7ea2e6769904b61359a48a5c5e45c8d9
Andrew LaCharite <adlacharite@gmail.com>
2020-03-28 20:25:49 +0000

    add xterm

--- deps.sh
+++ deps.sh
@@ -1,5 +1,5 @@
 
-apt install lynx libglu1-mesa-dev libgtk2.0-0
+apt install lynx libglu1-mesa-dev libgtk2.0-0 xterm
 apt-get install libsdl1.2debian libsdl-image1.2 libsdl-ttf2.0-0 libopenal1 libsndfile1 libncursesw5
 lynx -cmd_script=getdf "http://dffd.bay12games.com/file.php?id=13244"
 
